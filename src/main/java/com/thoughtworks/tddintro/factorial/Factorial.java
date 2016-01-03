093a2baafe7556ed58e7741837d1687ccc3b0222
Aidan Obley <aidan@obley.net>
2016-01-03 16:07:25 -0500

    Recursive reuse, Fourth test passing

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -4,9 +4,7 @@ public class Factorial {
     public Integer compute(int i) {
         if (i == 1 || i == 0) {
             return 1;
-        } else if (i == 2) {
-            return 2;
         }
-        return -1;
+        return i * compute(i-1);
     }
 }
