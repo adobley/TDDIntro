73c592683208cc04214ed4fb40b49bd04b059e02
Aidan Obley <aidan@obley.net>
2016-01-03 16:09:02 -0500

    Fifth test passing, factorial tests done

--- src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/factorial/Factorial.java
@@ -2,6 +2,9 @@ package com.thoughtworks.tddintro.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        if (i < 0) {
+            throw new IllegalArgumentException();
+        }
         if (i == 1 || i == 0) {
             return 1;
         }
