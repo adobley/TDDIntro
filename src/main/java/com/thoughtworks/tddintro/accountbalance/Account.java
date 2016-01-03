a18fc68aaa6b651a21d3eea4aad91319e25722e9
Aidan Obley <aidan@obley.net>
2016-01-03 16:21:06 -0500

    First Account test passing

--- src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -4,13 +4,17 @@ package com.thoughtworks.tddintro.accountbalance;
  * Created by adobley on 1/3/16.
  */
 public class Account {
-    public Account(int i) {
+    private int balance;
+
+    public Account(int balance) {
+        this.balance = balance;
     }
 
-    public void deposit(int i) {
+    public void deposit(int deposit) {
+        balance += deposit;
     }
 
     public int balance() {
-        return -1;
+        return this.balance;
     }
 }
