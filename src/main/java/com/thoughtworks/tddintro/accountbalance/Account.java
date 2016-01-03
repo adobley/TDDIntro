d59c497dadc6996c710790a62aedb4619e5e3bdf
Aidan Obley <aidan@obley.net>
2016-01-03 16:19:11 -0500

    Wrote first Account test, added account class, test failing

---
+++ src/main/java/com/thoughtworks/tddintro/accountbalance/Account.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.tddintro.accountbalance;
+
+/**
+ * Created by adobley on 1/3/16.
+ */
+public class Account {
+    public Account(int i) {
+    }
+
+    public void deposit(int i) {
+    }
+
+    public int balance() {
+        return -1;
+    }
+}
